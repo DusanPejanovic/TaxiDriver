88f10af8b84a9aef1a8f81a62ec2f884e779c002
Ilija Stanojević <stanojevic.sv71.2020@uns.ac.rs>
2022-11-13 01:28:15 +0100

    Finish layout for passenger account

--- src/app/src/main/java/com/example/taxidriver/PassengerAccountActivity.java
+++ src/app/src/main/java/com/example/taxidriver/PassengerAccountActivity.java
@@ -4,6 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+//6. PassengerAccountActivity - omogućiti prikaz menija sa sledećim stavkama:
+//        6.1. Nalog: pregled informacija o nalogu putnika i njihovo ažuriranje:
+//        dodavanje profilne slike, izmena podataka o plaćanju, izmena osnovnih
+//        informacija (ime, prezime, lokacija..)
+//        6.2. Omiljene rute: prikaz omiljenih ruta od korisnika, odakle može da ponovo
+//        izabere istu rutu za poručivanje odmah ili kasnije, kao i da je obriše iz
+//        omiljenih.
+//        6.3. Izveštaj: mogućnost generisanja izveštaja o prethodnim vožnjama za
+//        definisan vremenski period. Na osnovu definisanog opsega datuma
+//        prikaže se graf koji prikazuje broj vožnji po danima, broj pređenih
+//        kilometara, količinu potrošenog novca za sopstvene vožnje. Uz te p
+
 public class PassengerAccountActivity extends AppCompatActivity {
 
     @Override
