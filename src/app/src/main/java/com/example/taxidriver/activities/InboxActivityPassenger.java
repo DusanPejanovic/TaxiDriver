47b1bd8e3f911ad8b26da9ad9df31d636e1c98fc
markop39 <96690196+markop39@users.noreply.github.com>
2022-11-14 12:55:11 +0100

    Passenger inbox design only lacking mock up data

---
+++ src/app/src/main/java/com/example/taxidriver/activities/InboxActivityPassenger.java
@@ -0,0 +1,15 @@
+package com.example.taxidriver.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.taxidriver.R;
+import android.os.Bundle;
+
+public class InboxActivityPassenger extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inbox_passenger);
+
+    }
+}
