75685b95b233760ab79ccc5259810befb2246041
DusanPejanovic <pejanovic.dusan2001@gmail.com>
2022-11-14 12:53:24 +0100

    added drive ride history

---
+++ src/app/src/main/java/com/example/taxidriver/activities/DriverRideHistoryActivity.java
@@ -0,0 +1,17 @@
+package com.example.taxidriver.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.taxidriver.R;
+
+
+public class DriverRideHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_ride_history);
+    }
+}
