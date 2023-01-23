535c775c838e3c83a85a4a849cdfa458fe743f8b
DusanPejanovic <pejanovic.dusan2001@gmail.com>
2023-01-23 16:22:21 +0100

    finished splash activity

--- src/app/src/main/java/com/example/taxidriver/ui/activities/SplashActivity.java
+++ src/app/src/main/java/com/example/taxidriver/ui/activities/SplashActivity.java
@@ -3,7 +3,9 @@ package com.example.taxidriver.ui.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.widget.Toast;
 
 import com.example.taxidriver.R;
@@ -20,23 +22,36 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        int SPLASH_TIME_OUT = 1500;
 
-        if (Tools.getConnectivityStatus(getApplicationContext()) == 0) {
-            Toast.makeText(TaxiDriver.getAppContext(), "Turn on wifi or mobile data.", Toast.LENGTH_SHORT).show();
-            SPLASH_TIME_OUT = 10000;
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        boolean isLocationEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        if (!isLocationEnabled) {
+            Toast.makeText(this, "Location services are required for this app to function properly", Toast.LENGTH_LONG).show();
+            Intent settingsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(settingsIntent);
+            finish();
+        } else {
+
+            int SPLASH_TIME_OUT = 1500;
+
+            if (Tools.getConnectivityStatus(getApplicationContext()) == 0) {
+                Toast.makeText(TaxiDriver.getAppContext(), "Turn on wifi or mobile data.", Toast.LENGTH_SHORT).show();
+                SPLASH_TIME_OUT = 10000;
+            }
+
+            new Timer().schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    if (Tools.getConnectivityStatus(getApplicationContext())  != 0) {
+                        startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                    }
+                    finish();
+                }
+            }, SPLASH_TIME_OUT);
         }
 
-        new Timer().schedule(new TimerTask() {
-            @Override
-            public void run() {
-                int status = Tools.getConnectivityStatus(getApplicationContext());
-                if (Tools.getConnectivityStatus(getApplicationContext())  != 0) {
-                    startActivity(new Intent(SplashActivity.this, LoginActivity.class));
-                }
-                finish();
-            }
-        }, SPLASH_TIME_OUT);
+
 
 
     }
